cb30072c459a4d54afad81d6f71010773aca2a6b
Mihail <pauk.misha@bk.ru>
2021-08-09 16:40:39 +0000

    Created Lesson9

---
+++ Lesson9/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+/*Реализовать функцию, которая возвращает сумму цифр в
+ переданной ей строке. str_sum_digits(const char *cs).
+ Написать программу для проверки данной функции*/
+
+int main(int argc, char **argv)
+{
+
+
+    return 0;
+}

9c48e22a5132af1c14fbd1b383a2edc94971f11c
root <pauk.misha@bk.ru>
2021-08-10 20:16:40 +0000

    main.c

--- Lesson9/main.c
+++ Lesson9/main.c
@@ -9,9 +9,42 @@
  переданной ей строке. str_sum_digits(const char *cs).
  Написать программу для проверки данной функции*/
 
+void my_str_sum_digits(const char *cs);
+void get_char(void);
+
 int main(int argc, char **argv)
-{
+{char numbers[256] = { 0 };
+    char c = 0;
+    int i = 0;
+
+ printf("Введите любые числа\n");
+        
+        while((numbers[i] = getchar()) != '\n')
+        {
+            if(numbers[i] >= '1' && c <= '9'){
+                printf("%c\n", numbers[i]);
+                i++;  
+            }else{
+                printf("%s", "В строке есть символы!\n");
+                break;
+            }
+        }
+        my_str_sum_digits(numbers);
+    }
 
 
-    return 0;
+
+void my_str_sum_digits(const char *cs){
+
+    int summ = 0;
+    
+    for(int i = 0; i<cs[256] ; i++){
+
+         if(cs[i] != 0 && cs[i] != 10){
+            summ += (cs[i] - 48);
+            printf("%d\n", summ);
+         }
+    }
+    
+    printf("%d\n", summ);
 }
